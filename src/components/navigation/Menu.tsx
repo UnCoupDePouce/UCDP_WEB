1e8282adbc0ef1e1e8e80ae332fbe908420bcafb
Thomas <thomas.zabalo@gmail.com>
2025-12-29 12:07:27 +0100

    change folder

--- src/components/navigation/Menu.tsx
+++ src/components/navigation/Menu.tsx
@@ -1,17 +1,14 @@
 import {NavLink} from "react-router";
-import {menuItems} from "../data/menu.ts";
+import {menuItems} from "../../data/menu.ts";
 
 function Menu() {
 
     return (
-        <div className="flex flex-col w-72 z-10 inset-0 fixed bg-gray-900">
-            <div className="flex grow flex-col gap-y-5 overflow-y-auto border-r-1 border-white/10 bg-white/10 px-6">
-                <div className="flex items-center shrink-0 h-16">
-                    <img src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&amp;shade=500"
-                         alt="Your Company" className="w-auto h-8"/>
-                </div>
-                <nav className="flex flex-1 flex-col">
-                    <ul role="list" className="flex flex-1 flex-col gap-y-7">
+        <div className="col-start-1 row-span-2 row-start-1 max-2xl:hidden">
+            <div
+                className="sticky top-14 z-10 flex h-full max-h-[calc(100dvh---spacing(14))] flex-col bg-white dark:bg-gray-950">
+                <div className="flex-1 overflow-y-auto p-6">
+                    <ul role="list" className="flex flex-col gap-8">
                         <li>
                             <ul role="list" className="flex flex-1 flex-col gap-y-7">
                                 <li>
@@ -22,7 +19,7 @@ function Menu() {
                                                 <li key={item.to}>
                                                     <NavLink
                                                         to={item.to}
-                                                        className={({ isActive }) =>
+                                                        className={({isActive}) =>
                                                             `flex items-center gap-x-3 p-2 rounded-md text-sm font-semibold ${
                                                                 isActive ? "bg-white/5 text-white" : "text-gray-500 hover:bg-white/5"
                                                             }`
@@ -42,7 +39,7 @@ function Menu() {
                             </ul>
                         </li>
                     </ul>
-                </nav>
+                </div>
             </div>
         </div>
     )
